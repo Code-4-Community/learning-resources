ec2d2f2ec5a55c033540aac5ea3b9beffccc923e
Liam Moynihan <liammoyn@gmail.com>
2019-10-27 19:23:50 -0400

    Added actual buttonclickerapp files

---
+++ workshop2/buttonclickerapp/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <ButtonCounter description='Times Ive stuttered' />
+    <ButtonCounter description='Times Ive been wrong' />
+    <ButtonCounter description='Buttons Ive created' />
+  </div>
+</template>
+
+<script>
+import ButtonCounter from './components/ButtonCounter.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ButtonCounter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ workshop2/buttonclickerapp/src/components/ButtonCounter.vue
@@ -0,0 +1,32 @@
+
+<template>
+    <div  class="container">
+        <p>This Button tracks: {{description}}</p>
+        <button v-on:click="count++">This button's been clicked {{ count }} times.</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ButtonCounter',
+    props: ['description'],
+    data: function() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    border-radius: 10%;
+    color: red;
+}
+
+.container {
+    border-color: black;
+    border-width: 5px;
+    border-style: solid;
+}
+</style>
